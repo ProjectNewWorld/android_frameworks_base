b61bc9e3b815473b2840aa4c58b446bbb2416882
cristianomatos <cristianobmatos@gmail.com>
2018-01-11 20:49:15 +0100

    Recents Clear All Button Tweaks and Add FAB to Recents Panel  Thanks to RR and @varund7726
    Extracted, cleaned and updated for Nitrogen OS by @xyyx
    
    # Conflicts:
    #	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3965,6 +3965,18 @@ public final class Settings {
         public static final int SHOW_LOCKSCREEN_VISUALIZER_DEFAULT = 1;
 
         /**
+        * show clear all recents button
+        * @hide
+        */
+        public static final String SHOW_CLEAR_ALL_RECENTS = "show_clear_all_recents";
+
+        /**
+         * location of the clear all rectents button
+         * @hide
+         */
+        public static final String  RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
